99af1b45b4d972877cf167c21f06cc77e9acd5e8
Veeresh2708 <41538010+Veeresh2708@users.noreply.github.com>
2018-10-09 21:55:09 +0530

    Update Testjob1.groovy

--- Testjob1.groovy
+++ Testjob1.groovy
@@ -4,6 +4,11 @@ label("free")
       logRotator(-1, 10)
       jdk('Java 6')
       scm {
-        github("jenkinsci/job-dsl-plugin","master")
+            git {
+            remote {
+                name('origin')
+                url('git@serverA:account/repo1.git')
+            }
+        }
     }
 }
